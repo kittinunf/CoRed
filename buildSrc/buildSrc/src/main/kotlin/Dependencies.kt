d6b056214c7d60eb1c2c423d53a01ffd317f9f37
Kittinun Vantasin <kittinun.f@gmail.com>
2022-05-02 19:43:40 +0900

    Update to 1.6.21

--- buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 // Main libraries
 object Kotlin {
 
-    private const val version = "1.6.10"
+    private const val version = "1.6.21"
 
     const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
 
