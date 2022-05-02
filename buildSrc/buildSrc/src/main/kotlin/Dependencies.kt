14199c49026b437f701dbce27337cbe7c33818d8
Kittinun Vantasin <kittinun.f@gmail.com>
2022-05-02 19:48:04 +0900

    Update kotlin coroutine

--- buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/buildSrc/src/main/kotlin/Dependencies.kt
@@ -28,7 +28,7 @@ object AndroidX {
 
 object Coroutines {
 
-    private const val version = "1.6.0"
+    private const val version = "1.6.1"
 
     const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
     const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
